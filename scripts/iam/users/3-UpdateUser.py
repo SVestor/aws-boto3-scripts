9172dcf8ebfc68ed6a98cb680b7672ecc35af18f
SV <cloud@svestor.link>
2025-05-27 00:55:04 +0300

    boto3: update user

---
+++ scripts/iam/users/3-UpdateUser.py
@@ -0,0 +1,13 @@
+import boto3
+
+def update_user(old_user_name, new_user_name):
+    client = boto3.client('iam')
+    response = client.update_user(
+        UserName=old_user_name, 
+        NewUserName=new_user_name)
+    
+    print(response)
+    
+if __name__ == '__main__':
+    update_user('test-user', 'test-user_updated')
+    
