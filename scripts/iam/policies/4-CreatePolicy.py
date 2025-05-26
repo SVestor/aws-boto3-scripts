5c6804534e24153bd85ced8c6ddff162692e539a
SV <cloud@svestor.link>
2025-05-27 00:57:38 +0300

    boto3: create policy

---
+++ scripts/iam/policies/4-CreatePolicy.py
@@ -0,0 +1,19 @@
+import boto3
+import json
+
+
+def create_policy(policy_name, policy_document_path):
+    client = boto3.client('iam')
+
+    with open(policy_document_path, 'r') as f:
+        policy = json.load(f)  # Load as Python dict
+
+    response = client.create_policy(
+        PolicyName=policy_name,
+        PolicyDocument=json.dumps(policy)  # Convert to JSON string
+    )
+    print(response)
+
+
+if __name__ == '__main__':
+    create_policy('s3_list_bucket', './policies/s3_list_bucket.json')
