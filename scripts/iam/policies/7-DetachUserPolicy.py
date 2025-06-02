937e7b4be25d91a4cd28c5c711bb5e0eb067746d
SV <cloud@svestor.link>
2025-06-02 22:48:39 +0300

    boto3: detach user policy

---
+++ scripts/iam/policies/7-DetachUserPolicy.py
@@ -0,0 +1,14 @@
+import boto3
+
+def detach_policy_from_user(user_name, policy_arn):
+    client = boto3.client('iam')
+    response = client.detach_user_policy(
+        UserName=user_name,
+        PolicyArn=policy_arn
+    )
+    print(response)
+    
+if __name__ == '__main__':
+    policy_arn = "arn:aws:iam::339712849641:policy/s3_list_bucket"
+    detach_policy_from_user('test-user5', policy_arn)
+    
