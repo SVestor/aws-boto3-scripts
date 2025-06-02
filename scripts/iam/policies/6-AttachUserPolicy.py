8e82d6addd684d1cb59e911976b3a7d85875eb3d
SV <cloud@svestor.link>
2025-06-02 22:40:58 +0300

    boto3: attach user policy

---
+++ scripts/iam/policies/6-AttachUserPolicy.py
@@ -0,0 +1,14 @@
+import boto3
+
+def attach_policy_to_user(user_name, policy_arn):
+    client = boto3.client('iam')
+    response = client.attach_user_policy(
+        UserName=user_name,
+        PolicyArn=policy_arn
+    )
+    print(response)
+    
+
+if __name__ == '__main__':
+    policy_arn = "arn:aws:iam::339712849641:policy/s3_list_bucket"
+    attach_policy_to_user('test-user5', policy_arn)
