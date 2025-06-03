a4bc317725e3c96e983090df75aec6e2e01469c7
SV <cloud@svestor.link>
2025-06-03 23:32:12 +0300

    boto3: delete login profile

---
+++ scripts/iam/users/11-DeleteLoginProfile.py
@@ -0,0 +1,10 @@
+import boto3
+
+def delete_login_profile(user_name):
+    client = boto3.client('iam')
+    response = client.delete_login_profile(UserName=user_name)
+    print(response)
+    
+if __name__ == '__main__':
+    delete_login_profile('test-user5')
+      
