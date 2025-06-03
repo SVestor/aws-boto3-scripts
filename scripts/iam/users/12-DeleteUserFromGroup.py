983d6555df5d5106724e5c13a9392b89895eb397
SV <cloud@svestor.link>
2025-06-03 23:59:52 +0300

    boto3: delete user from group // using resource

--- scripts/iam/users/12-DeleteUserFromGroup.py
+++ scripts/iam/users/12-DeleteUserFromGroup.py
@@ -7,7 +7,22 @@ def delete_user_from_group(user_name, group_name):
         GroupName=group_name
     )
     print(response)
+
+# Delete user from group using resource
+# Resource is more pythonic way to interact with AWS (high level abstractions)
+
+def delete_user_from_group_v2(user_name, group_name):
+    iam = boto3.resource('iam')
+    user = iam.User(user_name)
+    response = user.remove_group(GroupName=group_name)
+    print(response)
+
+# Another way to delete user from group using resource
+#     group = iam.Group(group_name)
+#     response = group.remove_user(UserName=user_name)
+#     print(response)
     
 if __name__ == '__main__':
-    delete_user_from_group('test-user5', 'test-group')
-      
+    # delete_user_from_group('test-user5', 'test-group')
+    delete_user_from_group_v2('test-user5', 'test-group')
+ 
