0052e336c57be6dd17922f96af0b3ce5ceb246be
SV <cloud@svestor.link>
2025-06-03 23:31:22 +0300

    boto3: delete access key

---
+++ scripts/iam/users/9-DeleteAccessKey.py
@@ -0,0 +1,13 @@
+import boto3
+
+def delete_access_key(user_name, access_key_id):
+    client = boto3.client('iam')
+    response = client.delete_access_key(
+        UserName=user_name,
+        AccessKeyId=access_key_id
+    )
+    print(response)
+    
+if __name__ == '__main__':
+    delete_access_key('test-user5', 'AKIAZI2LH2O4VNTWDMXZ')
+      
