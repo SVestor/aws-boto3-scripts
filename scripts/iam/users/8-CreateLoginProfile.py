6afeaf42138c5086bf6462d4c1c550112e87a1bf
SV <cloud@svestor.link>
2025-06-03 23:15:33 +0300

    boto3: create login profile for user

---
+++ scripts/iam/users/8-CreateLoginProfile.py
@@ -0,0 +1,13 @@
+import boto3
+
+def create_login_profile(user_name, password):
+    client = boto3.client('iam')
+    response = client.create_login_profile(
+        UserName=user_name,
+        Password=password,
+        PasswordResetRequired=True
+    )
+    print(response)
+    
+if __name__ == '__main__':
+    create_login_profile('test-user5', 'Test123!')
