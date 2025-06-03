67ee88c972958dd86179eeb378139d5da5cf556a
SV <cloud@svestor.link>
2025-06-03 23:33:22 +0300

    boto3: delete user

---
+++ scripts/iam/users/10-DeleteUser.py
@@ -0,0 +1,10 @@
+import boto3
+
+def delete_user(user_name):
+    client = boto3.client('iam')
+    response = client.delete_user(UserName=user_name)
+    print(response)
+    
+if __name__ == '__main__':
+    delete_user('test-user5')
+      
