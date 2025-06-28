95bce6facc3f05cd8983756ba8dcd80a4fa7efc4
SV <cloud@svestor.link>
2025-06-28 21:08:28 +0300

    boto3: add tag

---
+++ scripts/iam/users/13-AddTag.py
@@ -0,0 +1,20 @@
+import boto3
+
+
+def add_tag_to_user(user_name, tag_key, tag_value):
+    client = boto3.client('iam')
+    response = client.tag_user(
+        UserName=user_name,
+        Tags=[
+            {
+                'Key': tag_key,
+                'Value': tag_value
+            }
+        ]
+    )
+    print(f"Tag {tag_key} with value {tag_value} added to user {user_name}")
+    
+
+if __name__ == '__main__':
+    add_tag_to_user('test-user5', 'Department', 'IT')
+    add_tag_to_user('test-user5', 'Project', 'Test')
