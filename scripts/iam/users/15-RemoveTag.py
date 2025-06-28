49a040be28b15cab25cbc93209a72cecf3ba4aa6
SV <cloud@svestor.link>
2025-06-28 21:09:57 +0300

    boto3: remove tag from a user

---
+++ scripts/iam/users/15-RemoveTag.py
@@ -0,0 +1,17 @@
+import boto3
+
+def remove_tag_from_user(user_name, tag_key):
+    client = boto3.client('iam')
+    tags = client.list_user_tags(UserName=user_name)['Tags']
+    for tag in tags:
+        if tag['Key'] == tag_key:
+            value = tag['Value']
+            response = client.untag_user(
+                UserName=user_name,
+                TagKeys=[tag_key]
+            )
+            print(f"Tag {tag_key} with value {value} was successfully removed from user {user_name}")
+    
+if __name__ == '__main__':
+    remove_tag_from_user('test-user5', 'Department')
+    remove_tag_from_user('test-user5', 'Project')
