5384ef907c3d272e6cff8198044debcd0f64fe7e
SV <cloud@svestor.link>
2025-11-02 22:27:44 +0200

    boto3: remove Tag

--- scripts/iam/users/15-RemoveTag.py
+++ scripts/iam/users/15-RemoveTag.py
@@ -13,5 +13,5 @@ def remove_tag_from_user(user_name, tag_key):
             print(f"Tag {tag_key} with value {value} was successfully removed from user {user_name}")
     
 if __name__ == '__main__':
-    remove_tag_from_user('test-user5', 'Department')
-    remove_tag_from_user('test-user5', 'Project')
+    remove_tag_from_user('test-user', 'Department')
+    remove_tag_from_user('test-user', 'Project')
